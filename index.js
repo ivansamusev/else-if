76e3618cba563e0b03ecfffadca9d864ee3cef8f
ivan <ivansamusev044@gmail.com>
2023-01-12 18:46:27 +0200

    if/ternar

---
+++ index.js
@@ -0,0 +1,15 @@
+let age = 17
+
+let message 
+
+
+if (age >= 18){
+    message = ' You are an adult '
+    console.log(message)
+    }else{
+        message = 'You are a minor'
+        console.log(message)
+    }
+
+
+
---
+++ ternar.js
@@ -0,0 +1,3 @@
+const age  = 18
+const message = age  >= 18 ? 'Вам 18' : 'Вам немає 18'
+console.log(message)
